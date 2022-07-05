b241d07590d58efe1574fafa8f02d30135927c4c
natali09yarmolik <85163434+natali09yarmolik@users.noreply.github.com>
2022-07-05 17:05:56 +0300

    git commit -m "init: creat "
    -m "feat: add file"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let str=String(Math.abs(n));
+    let strNew="";
+    for(let i=str.length-1; i>=0; i--)
+    {
+      strNew+=str[i];
+    }
+    let strNew1=Number(strNew);
+    return strNew1;
 }
